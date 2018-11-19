e6bf3d4d99cf0139ca5ae7e42b0415b11b6ece07
Michael <702422317@qq.com>
2018-11-19 16:14:31 +0800

    updated 11/16

--- network/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul  2 13:33:04 2018
-
-@author: Administrator
-"""
-
--- network/init.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul  6 08:37:19 2018
-
-@author: Administrator
-"""
-
